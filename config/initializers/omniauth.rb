c389c16612fe21ef3e0e44cea28ee4ee2d5c797d
Jill Donohue <jilldonohue@Jills-MacBook-4.local>
2015-12-16 13:56:42 -0700

    fix develop keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :twitter, ENV["PROD_TWITTER_KEY"], ENV["PROD_TWITTER_SECRET"]
 end
