63944cda99163daad50bae410fa64a3714b34d96
Jill Donohue <jillmd501@gmail.com>
2015-12-17 10:17:47 -0700

    testing

--- test/services/witter_service_test.rb
+++ test/services/witter_service_test.rb
@@ -4,20 +4,64 @@ class WitterServiceTest < ActiveSupport::TestCase
 
   attr_reader :service
 
-  def setup
-    user = OpenStruct.new(oauth_token: ENV["oauth_token"], oauth_token_secret: ENV["oauth_token_secret"])
-    @service = WitterService.new(user)
+def setup
+   user = OpenStruct.new(oauth_token: ENV["oauth_token"], oauth_token_secret: ENV["oauth_token_secret"])
+   @service = WitterService.new(user)
+ end
+
+ test "#my_tweets" do
+   VCR.use_cassette("witter_service#my_tweets") do
+     tweets = service.user_timeline
+     tweet = tweets.first
+
+     assert_equal 20, tweets.count
+     assert tweet[:text]
+   end
+ end
+
+ test "#all_tweets" do
+   VCR.use_cassette("witter_service#all_tweets") do
+     tweets = service.home_timeline
+     tweet = tweets.first
+
+     assert_equal 20, tweets.count
+     assert tweet[:text]
+   end
   end
 
-  test "#user_timeline" do
-    VCR.use_cassette("witter_service#user_timeline") do
-    user_timeline = service.user_timeline
-    tweet = user_timeline.first
+ test "#followers" do
+   VCR.use_cassette("witter_service#followers") do
+     total = service.followers
 
-    assert_equal 20, user_timeline.count
-    assert_equal "Joni", tweet[:tweet]
+     assert_equal 175, total.count
+   end
+  end
+
+ test "#friends" do
+   VCR.use_cassette("witter_service#friends") do
+     total = service.friends
+
+     assert_equal 191, total.count
+   end
+  end
+
+  test "#tweets" do
+    VCR.use_cassette("witter_service#tweets") do
+      tweets = service.user_timeline
+      tweet = tweets.first
+
+      assert_equal 20, tweets.count
+      assert tweet[:text]
     end
   end
 
+  test "#create_tweet" do
+    skip
+    VCR.use_cassette("twitter_service#create_tweet") do
+      tweet_msg = "My tweet"
+      tweet = service.tweet(tweet_msg)
 
+      assert_equal "My tweet", tweet.text
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,39 @@ require 'rails/test_help'
 require 'minitest/pride'
 require 'webmock'
 require 'vcr'
+require 'simplecov'
+require 'capybara'
+require 'minitest/pride'
+require 'vcr'
+
+SimpleCov.start("rails")
+
+include Capybara::DSL
+
 
 class ActiveSupport::TestCase
   fixtures :all
+
   VCR.configure do |config|
     config.cassette_library_dir = "test/cassettes"
     config.hook_into :webmock
+  end
+
+def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: 'twitter',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Horace",
+          screen_name: "worace",
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
   end
 end
